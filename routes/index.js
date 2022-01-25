18a402c925f31fc1a539a002b10d02143b04ffd6
Rupali-Baravkar <rbaravkar490@gmail.com>
2022-01-25 22:48:01 +0530

    This is Login Page

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Medical inventory' });
+
+});
+
+router.get('/Login', function(req, res, next){
+  res.render('Login')
+})
+
+module.exports = router;
