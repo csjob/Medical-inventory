c3d63c29f8d8d1a2a1164407df1f9489fb90b59e
csjob <csjob78@gmail.com>
2022-01-27 19:09:49 +0530

    user dashbord

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Medical inventory' });
-
+  // res.render('index', { title: 'Medical inventory' });
+  res.render('dashboard');
 });
 
 router.get('/Login', function(req, res, next){
-  res.render('Login')
-})
+  res.render('Login');
+});
+
+// router.get('/dashboard', function(req, res, next){
+//   res.render('dashboard');
+// });
 
 module.exports = router;
